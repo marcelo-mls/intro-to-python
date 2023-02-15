938c510efd405fe0a307f1835b40e0a4d6f9a178
Marcelo Marques <marcelo-mls@hotmail.com>
2023-02-15 02:01:07 -0300

    refactor: atualiza a função find_average com o metodo sum

--- scr/average.py
+++ scr/average.py
@@ -1,4 +1,4 @@
-def find_average(numbers: list[int]) -> float:
+def find_average_with_for(numbers: list[int]) -> float:
     """Calcula a média aritmética dos valores contidos em uma lista"""
     total = 0
     for number in numbers:
@@ -7,5 +7,12 @@ def find_average(numbers: list[int]) -> float:
     return total / len(numbers)
 
 
+def find_average(numbers: list[int]) -> float:
+    """Calcula a média aritmética dos valores contidos em uma lista"""
+    return sum(numbers) / len(numbers)
+
+
+print(find_average_with_for([4, 20]))
+print(find_average_with_for([40, 2, 5, 5]))
 print(find_average([4, 20]))
 print(find_average([40, 2, 5, 5]))
